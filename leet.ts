faf16ea6518010b8cbff4d91d2cd6dfff28d5223
Laurynas <pjooklas@gmail.com>
2022-11-07 08:02:06 +0200

    ok

--- leet.ts
+++ leet.ts
@@ -2,27 +2,37 @@
 
 
 //  * Definition for singly-linked list.
- export  class ListNode {
-      val: number
-      next: ListNode | null
-      constructor(val?: number, next?: ListNode | null) {
-          this.val = (val===undefined ? 0 : val)
-          this.next = (next===undefined ? null : next)
-      }
-  }
+//  export  class ListNode {
+//       val: number
+//       next: ListNode | null
+//       constructor(val?: number, next?: ListNode | null) {
+//           this.val = (val===undefined ? 0 : val)
+//           this.next = (next===undefined ? null : next)
+//       }
+//   }
 
-function middleNode(head: ListNode | null): ListNode | null {
-    let mid = head;
-    let end = head;
+// function middleNode(head: ListNode | null): ListNode | null {
+//     let mid = head;
+//     let end = head;
     
-    while (end != null && end.next != null) {
-        mid = mid!.next;
-        end = end.next.next;
-    }
+//     while (end != null && end.next != null) {
+//         mid = mid!.next;
+//         end = end.next.next;
+//     }
 
-    return mid;
-};
+//     return mid;
+// };
 
 // *********************************************
 
-
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    if (ransomNote.length > magazine.length) return false
+	
+    for (let i = 0; i < ransomNote.length; i++) {
+        const letter = ransomNote.substring(i, i + 1)
+        if (magazine.indexOf(letter) == -1) return false
+        magazine = magazine.replace(letter, '')
+    }
+    
+    return true
+};
